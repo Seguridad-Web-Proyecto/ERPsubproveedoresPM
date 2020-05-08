0e25a024027d8e02e5baad458cdee310d0f9ccbe
camilo <jcamilovelandiab@gmail.com>
2020-05-08 18:41:03 -0500

    webservices de categorias y productos ya funcionan. También se hizo el cálculo de la ganancia para cada producto, así el cliente ve cada producto con su correspondiente precio de venta

---
+++ src/main/java/restapplication/service/ProductoFacadeREST.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restapplication.service;
+
+import restapplication.AbstractFacade;
+import entidades.Producto;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author jcami
+ */
+@Stateless
+@Path("productos")
+public class ProductoFacadeREST extends AbstractFacade<Producto> {
+
+    @PersistenceContext(unitName = "com.mycompany_ERPsubprovee_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    public ProductoFacadeREST() {
+        super(Producto.class);
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Producto find(@PathParam("id") Long id) {
+        Producto producto = super.find(id);
+        Producto p = aplicarGanancia(producto);
+        return p;
+    }
+    
+    @GET
+    @Override
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Producto> findAll() {
+        List<Producto> productos = super.findAll();
+        List<Producto> returnList = new ArrayList<>();
+        for(Producto p: productos){
+            p = aplicarGanancia(p);
+            returnList.add(p);
+        }
+        return returnList;
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Producto> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        List<Producto> productos = super.findRange(new int[]{from, to});
+        List<Producto> returnList = new ArrayList<>();
+        for(Producto p: productos){
+            p = aplicarGanancia(p);
+            //System.out.println(p);
+            returnList.add(p);
+        }
+        return returnList;
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+    private Producto aplicarGanancia(Producto producto){
+        int gananciaxproducto = ((int)producto.getGanancia().getPorcentaje())*(int)producto.getPrecioUnitario();
+        gananciaxproducto /= 100;
+        int nuevoPrecio = (int)producto.getPrecioUnitario() + gananciaxproducto;
+        Producto productoQ = new Producto(producto.getProductoid(), producto.getNombre(), 
+                producto.getDescripcion(), producto.getUnidadMedida(), nuevoPrecio);
+        productoQ.setCategoriaid(producto.getCategoriaid());
+        return productoQ;
+    }
+    
+}
